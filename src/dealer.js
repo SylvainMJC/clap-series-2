a3ff47da7fe001e66fab64f3117106f8b8bcccc9
Sylvain <sylvain.mj.conan@gmail.com>
2020-06-08 11:41:16 +0200

    dealer.js

--- src/dealer.js
+++ src/dealer.js
@@ -12,6 +12,57 @@
  */
 function dealer() {
   // CODE HERE
+
+  let valeurs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  let types = ['d', 'c', 'h', 's'];
+  var cards1 = new Array();
+  var cards2 = new Array();
+  //console.log('cards1 :');
+  while(cards1.length<5){
+    let valeur1 = valeurs[Math.floor(Math.random() * valeurs.length)];
+    let type1 = types[Math.floor(Math.random() * types.length)];
+    let card1 = valeur1 + type1;
+    cards1[cards1.length]=card1;
+    cards1 = removeDuplicates(cards1);
+    //console.log(cards1);
+  }
+  //console.log(cards1);
+  //console.log('cards2 :');
+
+  while(cards2.length<5){
+    let valeur2 = valeurs[Math.floor(Math.random() * valeurs.length)];
+    let type2 = types[Math.floor(Math.random() * types.length)];
+    let card2=valeur2+type2;
+    cards2[cards2.length]=card2;
+    cards2 = removeDuplicates(cards2);
+
+
+    //console.log(cards2);
+
+  }
+  
+  //console.log(cards2);
+  let hands = [ cards1, cards2 ];
+  return hands;
 }
 
-export { dealer };
+function removeDuplicates(colors) {
+  let unique = {};
+  colors.forEach(function(i) {
+    if(!unique[i]) {
+      unique[i] = true;
+    }
+  });
+  return Object.keys(unique);
+}
+
+export { dealer, removeDuplicates };
+//dealer();
+
+/*
+let tab = [ '2d', '2h', '2h', '5s', '10s' ];
+console.log(tab);
+tab = removeDuplicates(tab);
+tab = removeDuplicates(cards2);
+console.log(tab);
+*/
